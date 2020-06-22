2110efe9efa29dc60ad31831bf6b82a4ea27efde
Gwey-Masao <masao.guei@force-corp.co.jp>
2020-06-22 15:17:22 +0900

    commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -19,7 +19,7 @@ import Dashboard from "../../pages/dashboard";
 import Typography from "../../pages/typography";
 import Notifications from "../../pages/notifications";
 import Maps from "../../pages/maps";
-// import Tables from "../../pages/tables";
+import Tables from "../../pages/tables";
 import Icons from "../../pages/icons";
 import Charts from "../../pages/charts";
 
@@ -60,8 +60,8 @@ function Layout(props) {
             <Switch>
               <Route path="/app/dashboard" component={Dashboard} />
               <Route path="/app/typography" component={Typography} />
-              {/* <Route path="/app/tables" component={Tables} /> */}
-              <Route path="/app/tables" component={StaffSearch_G} />
+              <Route path="/app/tables" component={Tables} />
+              {/* <Route path="/app/tables" component={StaffSearch_G} /> */}
               <Route path="/app/notifications" component={Notifications} />
 
               {/* <Route path="/staff-report/stafflist/001" component={StaffList001} /> */}
