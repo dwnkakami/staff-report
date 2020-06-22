bbf526d6104cc8416255a28fc2c86e81e5805236
kodamakun <tomomasa.kodama@force.co.jp>
2020-06-22 14:18:46 +0900

    Menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -58,7 +58,7 @@ function Layout(props) {
           >
             <div className={classes.fakeToolbar} />
             <Switch>
-              <Route path="/app/dashboard" component={Dashboard} />
+              <Route path="/staff-report/dashboard" component={Dashboard} />
               <Route path="/app/typography" component={Typography} />
               <Route path="/app/tables" component={Tables} />
               <Route path="/app/notifications" component={Notifications} />
