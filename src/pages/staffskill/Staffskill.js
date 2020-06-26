2fad311c23c1f0b4fa3e3abeec9f7a192ce47968
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-06-26 11:33:06 +0900

    mabuchi

--- src/pages/staffskill/Staffskill.js
+++ src/pages/staffskill/Staffskill.js
@@ -36,30 +36,31 @@ const useStyles = makeStyles((theme) => ({
   },
   name: {
     position: "absolute",
-    top: 130,
+    top: 140,
     left: 50,
     fontSize: 20,
   },
   buttongroup: {
     position: "absolute",
-    top: 160,
+    top: 170,
     left: 50,
+    
   },
   button1: {
     position: "absolute",
-    top:160,
+    top:170,
     left: 850,
   },
   button2: {
     position: "absolute",
-    top: 160,
+    top: 170,
     left: 1050,
   },
   list1: {
-    lineheight: 1 ,
+    margin: 0,
   },
   list2: {
-    lineheight: 1 ,
+    margin: 0,
     backgroundColor: "SkyBlue",
   },
   background: {
@@ -67,14 +68,47 @@ const useStyles = makeStyles((theme) => ({
     top: 200,
     width: 1150,
     left: 40,
+    margin: 0,
+   lineHeight: 30,
+ 
+  },
+  title1: {
+    position: "relative",
+    top: -13,
+    left: 50,
+  },
+  title2: {
+    position: "relative",
+    top: -52,
+    left: 450,
+  },
+  title3: {
+    position: "relative",
+    top: -92,
+    left: 850,
+  },
+  graph1: {
+    position: "relative",
+    top: -130,
+    left: 35,
+  },
+  graph2: {
+    position: "relative",
+    top: -310,
+    left: 440,
+  },
+  graph3: {
+    position: "relative",
+    top: -490,
+    left: 840,
   }
 }));
   //表示させたいデータ群
   const  data_event = [
-    { name: '国語', "点数": 1500 },
-    { name: '数学', "点数": 868 },
-    { name: '理科', "点数": 1397 },
-    { name: '社会', "点数": 1480 },
+    { name: '国語', "点数": 500 },
+    { name: '数学', "点数": 168 },
+    { name: '理科', "点数": 397 },
+    { name: '社会', "点数": 480 },
     { name: '英語', "点数": 20 },
   ]
 
@@ -89,16 +123,16 @@ export default function Variants() {
       </IconButton>
       <div className={classes.title}>{"スタッフ詳細"}</div>
       <div className={classes.name}>{"テスト 太郎"}</div>
-         <ButtonGroup className={classes.buttongroup} variant="contained" color="primary" aria-label="contained primary button group">
+         <ButtonGroup className={classes.buttongroup} variant="contained" color="gray" aria-label="contained primary button group">
          <Button>スキル</Button>
         <Button>経歴</Button>
         <Button>キャリアパス</Button>
         <Button>スタッフ情報</Button>
      </ButtonGroup>
-        <Button className={classes.button1} variant="contained" color="primary">
+        <Button className={classes.button1} variant="contained" color="gray">
          引き合い登録
        </Button>
-       <Button className={classes.button2} variant="contained" color="primary">
+       <Button className={classes.button2} variant="contained" color="gray">
          スキルシート出力
        </Button>
        <div className={classes.background}>
@@ -115,12 +149,16 @@ export default function Variants() {
          <List className={classes.list1}></List>
          <List className={classes.list2}></List>
        </div>
+       <h3 className={classes.title1}>スキル</h3>
+       <h3 className={classes.title2}>経験年数</h3>
+       <h3 className={classes.title3}>自己評価</h3>
+       <div className={classes.graph1}>
        <ComposedChart　　//グラフ全体のサイズや位置、データを指定。場合によってmarginで上下左右の位置を指定する必要あり。
-    width={600}  //グラフ全体の幅を指定
-    height={280}  //グラフ全体の高さを指定
+    width={350}  //グラフ全体の幅を指定
+    height={180}  //グラフ全体の高さを指定
     layout="vertical" //グラフのX軸とY軸を入れ替え
     data={data_event}   //Array型のデータを指定
-    margin={{ top: 20, right: 60, bottom: 0, left: 150 }}  //marginを指定
+    margin={{ top: 20, right: 60, bottom: 0, left: 0 }}  //marginを指定
   >
     <XAxis  //X軸に関する設定
       type="number" //データタイプをnumberに変更。デフォルトではcategoryになっている
@@ -143,6 +181,67 @@ export default function Variants() {
       fill="#2250A2"
     />
   </ComposedChart>
+  </div>
+  <div className={classes.graph2}>
+  <ComposedChart　　//グラフ全体のサイズや位置、データを指定。場合によってmarginで上下左右の位置を指定する必要あり。
+    width={350}  //グラフ全体の幅を指定
+    height={180}  //グラフ全体の高さを指定
+    layout="vertical" //グラフのX軸とY軸を入れ替え
+    data={data_event}   //Array型のデータを指定
+    margin={{ top: 20, right: 60, bottom: 0, left: 0 }}  //marginを指定
+  >
+    <XAxis  //X軸に関する設定
+      type="number" //データタイプをnumberに変更。デフォルトではcategoryになっている
+      domain={['dataMin - 100', 'dataMax + 10']} //軸の表示領域を指定
+    />
+    <YAxis //Y軸に関する設定
+      type="category" //データタイプをcategoryに変更
+      dataKey="name"  //Array型のデータの、Y軸に表示したい値のキーを指定
+    />
+    <Tooltip /> 
+    {/* hoverさせた時に具体的な値を表示させるように指定 */}
+    <CartesianGrid  //グラフのグリッドを指定
+      stroke="#f5f5f5"  //グリッド線の色を指定
+     /> 
+    <Bar
+      dataKey="点数"
+      barSize={20}
+      stroke="rgba(34, 80, 162, 0.2)"
+      fillOpacity={1}
+      fill="#2250A2"
+    />
+  </ComposedChart>
+  </div>
+  <div className={classes.graph3}>
+  <ComposedChart　　//グラフ全体のサイズや位置、データを指定。場合によってmarginで上下左右の位置を指定する必要あり。
+    width={350}  //グラフ全体の幅を指定
+    height={180}  //グラフ全体の高さを指定
+    layout="vertical" //グラフのX軸とY軸を入れ替え
+    data={data_event}   //Array型のデータを指定
+    margin={{ top: 20, right: 60, bottom: 0, left: 0 }}  //marginを指定
+  >
+    <XAxis  //X軸に関する設定
+      type="number" //データタイプをnumberに変更。デフォルトではcategoryになっている
+      domain={['dataMin - 100', 'dataMax + 10']} //軸の表示領域を指定
+    />
+    <YAxis //Y軸に関する設定
+      type="category" //データタイプをcategoryに変更
+      dataKey="name"  //Array型のデータの、Y軸に表示したい値のキーを指定
+    />
+    <Tooltip /> 
+    {/* hoverさせた時に具体的な値を表示させるように指定 */}
+    <CartesianGrid  //グラフのグリッドを指定
+      stroke="#f5f5f5"  //グリッド線の色を指定
+     /> 
+    <Bar
+      dataKey="点数"
+      barSize={20}
+      stroke="rgba(34, 80, 162, 0.2)"
+      fillOpacity={1}
+      fill="#2250A2"
+    />
+  </ComposedChart>
+  </div>
       </Paper>
     </div>
   );
