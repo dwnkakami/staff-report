3ce137a9e4ae0a43dbf49982e6670e96441105bb
jimufree <masahiro.kakami@jimu-free.com>
2020-07-02 13:41:07 +0900

    CaseSearch merged

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -34,7 +34,7 @@ import CaseSearch from "../../pages/casesearch/CaseSearch";
 //import CaseAdd from "../../pages/caseadd";
 
 import ReferenceList from "../../pages/referenceList/ReferenceList";
-// import ReferenceList_not from "../../pages/referenceList/notData";
+import ReferenceList_not from "../../pages/referenceList/notData";
 //import  from "../../pages/";　引合検索
 
 //import Billing from "../../pages/billing";
@@ -75,8 +75,8 @@ function Layout(props) {
               <Route path="/staff-report/casasearch/001" component={CaseSearch} />
               {/* <Route path="/staff-report/caseadd/001" component={CaseAdd} /> */}
 
-              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList} /> */}
-              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList_not} /> */}
+              <Route path="/staff-report/referencelist/001" component={ReferenceList} />
+              <Route path="/staff-report/referencelist/001" component={ReferenceList_not} />
               {/* <Route path="" component={} /> 引合検索  */}
               
               {/* <Route path="/staff-report/billing/001" component={Billing} /> */}
