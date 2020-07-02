b51623d29afdf270f201fb6e628ab027a2a2149e
shunichiro55 <63772173+shunichiro55@users.noreply.github.com>
2020-07-02 10:12:48 +0900

    Reference list takemura (#16)
    * test
    
    * test
    
    * test
    
    * data
    
    * design
    
    * file
    
    * file
    
    * line

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -31,7 +31,8 @@ import Charts from "../../pages/charts";
 //import CaseSearch from "../../pages/casesearch";
 //import CaseAdd from "../../pages/caseadd";
 
-//import ReferenceList from "../../pages/referencelist";
+import ReferenceList from "../../pages/referencelist/ReferenceList";
+// import ReferenceList_not from "../../pages/referenceList/notData";
 //import  from "../../pages/";　引合検索
 
 //import Billing from "../../pages/billing";
@@ -61,6 +62,7 @@ function Layout(props) {
               <Route path="/staff-report/dashboard" component={Dashboard} />
               <Route path="/app/typography" component={Typography} />
               <Route path="/app/tables" component={Tables} />
+              {/* <Route path="/app/tables" component={ReferenceList} /> */}
               <Route path="/app/notifications" component={Notifications} />
 
               {/* <Route path="/staff-report/stafflist/001" component={StaffList001} /> */}
@@ -71,7 +73,8 @@ function Layout(props) {
               {/* <Route path="/staff-report/casasearch/001" component={CaseSearch} /> */}
               {/* <Route path="/staff-report/caseadd/001" component={CaseAdd} /> */}
 
-              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList} /> */}
+              <Route path="/staff-report/referencelist/001" component={ReferenceList} />
+              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList_not} /> */}
               {/* <Route path="" component={} /> 引合検索  */}
               
               {/* <Route path="/staff-report/billing/001" component={Billing} /> */}
