e6a3c22223d1ea52ff026f3fe3bc245031aaaeb5
gennsou <ichigo5932@gmail.com>
2020-07-03 18:31:17 +0900

    commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -23,7 +23,7 @@ import Maps from "../../pages/maps";
 import Tables from "../../pages/tables";
 import Icons from "../../pages/icons";
 import Charts from "../../pages/charts";
-import StaffList001 from "../../pages/stafflist/StaffList001 copy";
+import StaffList001 from "../../pages/stafflist/StaffList001";
 
 import CaseSearch from "../../pages/casesearch/CaseSearch";
 
--- src/pages/stafflist/StaffList001 copy.jsx
+++ src/pages/stafflist/StaffList001 copy.jsx
@@ -1,238 +1,202 @@
-import React, { useState, useEffect, Component }　from 'react';
-import { render } from 'react-dom';
-import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-// import { getData }  from './M_staff';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-// import './StaffList001.css';
-import axios from 'axios';
+// import React, { useState, useEffect }　from 'react';
+// import Paper from '@material-ui/core/Paper';
+// import { Typography } from '@material-ui/core';
+// import { withStyles, makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// // import { getData }  from './M_staff';
+// import Grid from '@material-ui/core/Grid';
+// import Button from '@material-ui/core/Button';
+// // import './StaffList001.css';
+// import axios from 'axios';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.data
-    };
-  }
-  handleFilterVal(val) {
 
-    const line = this.props.data.filter((item) => (
-      item.id.toString().indexOf(val) >= 0
-      || item.name.toLowerCase().indexOf(val) >= 0
-    ));
-  
-    this.setState({
-      data: line
-    });
-  }
+//   function descendingComparator(a, b, orderBy) {
+//     if (b[orderBy] < a[orderBy]) {
+//       return -1;
+//     }
+//     if (b[orderBy] > a[orderBy]) {
+//       return 1;
+//     }
+//     return 0;
+//   }
 
-  handleSortByAscend(key) {
-    const line = this.state.data.sort((a, b) => {
-      if (a[key] < b[key]) return -1;
-      if (a[key] > b[key]) return 1;
-      return 0;
-    });
-    this.setState({
-      data: line
-    });
-  }
+//   function getComparator(order, orderBy) {
+//     return order === 'desc'
+//       ? (a, b) => descendingComparator(a, b, orderBy)
+//       : (a, b) => -descendingComparator(a, b, orderBy);
+//   }
 
-  handleSortByDescend(key) {
-    const line = this.state.data.sort((a, b) => {
-      if (a[key] < b[key]) return 1;
-      if (a[key] > b[key]) return -1;
-      return 0;
-    });
-    this.setState({
-        data: line
-    });
-  }
-render() {
+//   function stableSort(array, comparator) {
+//     const stabilizedThis = array.map((el, index) => [el, index]);
+//     stabilizedThis.sort((a, b) => {
+//       const order = comparator(a[0], b[0]);
+//       if (order !== 0) return order;
+//       return a[1] - b[1];
+//     });
+//     return stabilizedThis.map((el) => el[0]);
+//   }
+//   const headCells = [
+//     { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+//     { id: 'position', numeric: true, disablePadding: false, label: 'Position' },
+//     { id: 'company_abbrevation', numeric: true, disablePadding: false, label: 'Company_Abbrevation' },
+//     { id: 'matter_end', numeric: true, disablePadding: false, label: 'Matter_End' },
+//   ];
 
-  let list = this.state.data.map((data) => (
-    <li key={data.id}>
-    {data.id}: {data.name}
-    </li>
-  ));
-  return (
-    <div>
-      <Form onFilterVal={this.handleFilterVal.bind(this)} />
-      <SortButton
-      onSortByAscend={this.handleSortByAscend.bind(this)}
-      onSortByDescend={this.handleSortByDescend.bind(this)}
-      />
-      <ul>         
-        {list}
-      </ul>
-    </div>
-   );
-  }
-}
+//   function EnhancedTableHead(props) {
+//     const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+//     const createSortHandler = (property) => (event) => {
+//       onRequestSort(event, property);
+//     };
+//     return (
+//         <TableHead>
+//           <TableRow>
+//             {/* <TableCell padding="checkbox">
+//               <Checkbox
+//                 indeterminate={numSelected > 0 && numSelected < rowCount}
+//                 checked={rowCount > 0 && numSelected === rowCount}
+//                 onChange={onSelectAllClick}
+//                 inputProps={{ 'aria-label': 'select all desserts' }}
+//               />
+//             </TableCell> */}
+//             {headCells.map((headCell) => (
+//               <TableCell
+//                 key={headCell.id}
+//                 align={headCell.numeric ? 'right' : 'left'}
+//                 padding={headCell.disablePadding ? 'none' : 'default'}
+//                 sortDirection={orderBy === headCell.id ? order : false}
+//               >
+//                 <TableSortLabel
+//                   active={orderBy === headCell.id}
+//                   direction={orderBy === headCell.id ? order : 'asc'}
+//                   onClick={createSortHandler(headCell.id)}
+//                 >
+//                   {headCell.label}
+//                   {orderBy === headCell.id ? (
+//                     <span className={classes.visuallyHidden}>
+//                       {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+//                     </span>
+//                   ) : null}
+//                 </TableSortLabel>
+//               </TableCell>
+//             ))}
+//           </TableRow>
+//         </TableHead>
+//       );
+//     }
 
-App.propTypes = {
-  data: React.PropTypes
-};
+//     EnhancedTableHead.propTypes = {
+//         classes: PropTypes.object.isRequired,
+//         numSelected: PropTypes.number.isRequired,
+//         onRequestSort: PropTypes.func.isRequired,
+//         onSelectAllClick: PropTypes.func.isRequired,
+//         order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+//         orderBy: PropTypes.string.isRequired,
+//         rowCount: PropTypes.number.isRequired,
+//       };
 
-class Form extends Component {
-  _filterVal() {
-    const val = this.refs.myinput.value;
-    this.props.onFilterVal(val);
-}
-render() {
-  return (
-    <div>
-      <span style={{ marginRight: '8px', fontSize: '12px' }}>
-      キーワードで絞り込む:
-      </span>
-      <input
-      type="text"
-      ref="myinput"
-      defaultValue=""
-      onKeyUp={this._filterVal.bind(this)}
-      />
-      </div>
-   );
- }
-}
+//       EnhancedTableToolbar.propTypes = {
+//         numSelected: PropTypes.number.isRequired,
+//       };
 
-Form.propTypes = {
-  onFilterVal: React.PropTypes
-};
-class SortButton extends Component {
-  _sortByAscend(e) {
-    e.preventDefault();
 
-    this.props.handleSortByAscend(e.target.value);
-  }
+//  export default function StaffList001 () {
+//     const classes = useStyles();
+//     const [order, setOrder] = React.useState('asc');
+//     const [orderBy, setOrderBy] = React.useState('position');
+//     const [selected, setSelected] = React.useState([]);
+//     const [page, setPage] = React.useState(0);
+//     const [dense, setDense] = React.useState(false);
+//     const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  _sortByDescend(e) {
-    e.preventDefault();
+//     const handleRequestSort = (event, property) => {
+//         const isAsc = orderBy === property && order === 'asc';
+//         setOrder(isAsc ? 'desc' : 'asc');
+//         setOrderBy(property);
+//     };
+//     const [staff, setStaff] = useState([]);
+//     useEffect(() => getStaffData());
+//     const getStaffData = () => {
+//         if(staff.length === 0) {
+//             axios
+//               .get('/api/stafflist001/1')
+//               .then(response => {
+//                 setStaff([response.data]);
+//               })
+//               .catch(() => {
+//                 console.log('connected error');
+//               })
+//             }
+//     }
+    
+//     const handleSelectAllClick = (event) => {
+//         if (event.target.checked) {
+//           const newSelecteds = rows.map((n) => n.name);
+//           setSelected(newSelecteds);
+//           return;
+//         }
+//         setSelected([]);
+//       };
 
-    this.props.handleSortByDescend(e.target.value);
-  }
-  render() {
-    return (
-      <div>
-     <div>
-       <span style={{ marginRight: '8px', fontSize: '12px' }}>
-         idでソート:
-        </span>
-        <button onClick={this._sortByAscend.bind(this)} value="id">
-          昇順
-        </button>
-        
-        <button onClick={this._sortByDescend.bind(this)} value="id">
-          降順
-        </button>
-     </div>
-     <div>
-       <span style={{ marginRight: '8px', fontSize: '12px' }}>
-         nameでソート:
-       </span>
+//       const handleClick = (event, name) => {
+//         const selectedIndex = selected.indexOf(name);
+//         let newSelected = [];
+    
+//         if (selectedIndex === -1) {
+//           newSelected = newSelected.concat(selected, name);
+//         } else if (selectedIndex === 0) {
+//           newSelected = newSelected.concat(selected.slice(1));
+//         } else if (selectedIndex === selected.length - 1) {
+//           newSelected = newSelected.concat(selected.slice(0, -1));
+//         } else if (selectedIndex > 0) {
+//           newSelected = newSelected.concat(
+//             selected.slice(0, selectedIndex),
+//             selected.slice(selectedIndex + 1),
+//           );
+//         }
 
-        <button onClick={this._sortByAscend(this)} value="name">
-          昇順
-        </button>
-        <button onClick={this._sortByDescend.bind(this)} value="name">
-          降順
-        </button>
-        </div>
-        </div>
-    );
-    }
-  }
-  SortButton.propTypes = {
-    handleSortByAscend: React.PropTypes,
-    handleSortByDescend: React.PropTypes
-  };
-
-let data = [
-//   { id: 1, name: 'foo' },
-//   { id: 2, name: 'bar' },
-//   { id: 3, name: 'baz' },
-//   { id: 4, name: 'qux' },
-//   { id: 5, name: 'quux' },
-//   { id: 6, name: 'foobar' }
-];
-
-render(
-  <App data={data} />, document.querySelector('.content')
-);
-
-    const StyledTableCell = withStyles((theme) => ({
-    head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
-  const StyledTableRow = withStyles((theme) => ({
-    root: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-  }))(TableRow);
-  const useStyles = makeStyles({
-    table: {
-      minWidth: 700,
-    },
-  });
- export default function StaffList001 () {
-    const classes = useStyles();
-    const [staff, setStaff] = useState([]);
-    useEffect(() => getStaffData());
-    const getStaffData = () => {
-        if(staff.length === 0) {
-            axios
-              .get('/api/stafflist001/1')
-              .then(response => {
-                setStaff([response.data]);
-              })
-              .catch(() => {
-                console.log('connected error');
-              })
-            }
-    }
-return(
-<Grid>
-    <Typography variant="h2">スタッフリスト</Typography>
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>スタッフID</StyledTableCell>
-            <StyledTableCell align="right">スタッフ名</StyledTableCell>
-            <StyledTableCell align="right">役職</StyledTableCell>
-            <StyledTableCell align="right">所属会社</StyledTableCell>
-            <StyledTableCell align="right">案件終了日</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {staff.map((data) => (
-            <StyledTableRow key={data.id}>
-            <StyledTableCell component="th" scope="row">
-                {data.id}
-              </StyledTableCell>
-              <StyledTableCell align="right"><Button>{data.name}</Button></StyledTableCell>
-              <StyledTableCell align="right">{data.position}</StyledTableCell>
-              <StyledTableCell align="right">{data.company_abbreviation}</StyledTableCell>
-              <StyledTableCell align="right">{data.matter_end}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>      
-    </TableContainer>
-   </Grid>
-)
-};
+//         setSelected(newSelected);
+//     };
+//     const handleChangeDense = (event) => {
+//         setDense(event.target.checked);
+//       };
+    
+//       const isSelected = (name) => selected.indexOf(name) !== -1;
+    
+//       const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+// return(
+// <Grid>
+//     <Typography variant="h2">スタッフリスト</Typography>
+//     <TableContainer component={Paper}>
+//       <Table className={classes.table} aria-label="customized table">
+//         <TableHead>
+//           <TableRow>
+//             <StyledTableCell>スタッフID</StyledTableCell>
+//             <StyledTableCell align="right">スタッフ名</StyledTableCell>
+//             <StyledTableCell align="right">役職</StyledTableCell>
+//             <StyledTableCell align="right">所属会社</StyledTableCell>
+//             <StyledTableCell align="right">案件終了日</StyledTableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {staff.map((data) => (
+//             <StyledTableRow key={data.id}>
+//             <StyledTableCell component="th" scope="row">
+//                 {data.id}
+//               </StyledTableCell>
+//               <StyledTableCell align="right"><Button>{data.name}</Button></StyledTableCell>
+//               <StyledTableCell align="right">{data.position}</StyledTableCell>
+//               <StyledTableCell align="right">{data.company_abbreviation}</StyledTableCell>
+//               <StyledTableCell align="right">{data.matter_end}</StyledTableCell>
+//             </StyledTableRow>
+//           ))}
+//         </TableBody>
+//       </Table>      
+//     </TableContainer>
+//    </Grid>
+// )
+// };
