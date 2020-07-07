c6815e253862ce7c3f6a92e34e7c4b58ef80f6b9
gennsou <ichigo5932@gmail.com>
2020-07-07 18:35:39 +0900

    commit

--- src/pages/stafflist/StaffList001 copy.jsx
+++ src/pages/stafflist/StaffList001 copy.jsx
@@ -1,19 +1,103 @@
-// import React, { useState, useEffect }　from 'react';
+// import React, { useState, useEffect, Component }　from 'react';
+// import PropTypes from 'prop-types';
 // import Paper from '@material-ui/core/Paper';
 // import { Typography, DialogTitle } from '@material-ui/core';
-// import { lighten, withStyles, makeStyles } from '@material-ui/core/styles';
+// import { withStyles, makeStyles } from '@material-ui/core/styles';
 // import Table from '@material-ui/core/Table';
 // import TableBody from '@material-ui/core/TableBody';
 // import TableCell from '@material-ui/core/TableCell';
 // import TableContainer from '@material-ui/core/TableContainer';
 // import TableHead from '@material-ui/core/TableHead';
 // import TableRow from '@material-ui/core/TableRow';
+// import TableSortLabel from '@material-ui/core/TableSortLabel';
+// import PeopleIcon from '@material-ui/icons/People';
 // // import { getData }  from './M_staff';
 // import Grid from '@material-ui/core/Grid';
-// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import SearchIcon from '@material-ui/icons/Search';
 // // import './StaffList001.css';
 // import axios from 'axios';
 
+// class App extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             name: [
+//                 "佐藤テスト",
+
+//                 "後藤テスト",
+
+//                 "菊池テスト",
+
+//                 "磯野テスト",
+
+//                 "鈴木テスト",
+
+//                 "高橋テスト",
+
+//                 "田中テスト",
+
+//                 "伊藤テスト",
+
+//                 "渡邉テスト",
+
+//                 "中村テスト",
+
+//                 "中野テスト",
+
+//                 "田村テスト",
+
+//                 "石田テスト",
+
+//                 "安達テスト",
+
+//                 "井上テスト",
+
+//                 "テスト佐藤",
+
+//                 "テスト後藤",
+
+//                 "テスト菊池",
+
+//                 "テスト磯野",
+
+//                 "テスト鈴木",
+
+//                 "テスト高橋",
+
+//                 "テスト田中",
+
+//                 "テスト伊藤",
+
+//                 "テスト渡邉",
+
+//                 "テスト中村",
+
+//                 "テスト中野",
+
+//                 "テスト田村",
+
+//                 "テスト石田",
+
+//                 "テスト安達",
+
+//                 "テスト井上"
+//             ],
+//             items: []
+//         }
+//     }
+
+//     componentDidMount() {
+//         this.setState({items: this.state.name})
+//     }
+
+//     filterList = (e) => {
+//         const updateList = this.state.name.filter((item) => {
+//             return name.toLowerCase().search( e.target.value.toLowerCase()) !== -1;
+//         })
+//         this.setState({items: updateList})
+//     }
+// }
 
 // const StyledTableCell = withStyles((theme) => ({
 //     head: {
@@ -26,12 +110,18 @@
 //         fontSize: 14,
 //     },
 // }))(TableCell);
-
-// const useStyles = makeStyles({
-//     table: {
-//         minWidth: 700,
+// const StyledTableRow = withStyles((theme) => ({
+//     root: {
+//         '&:nth-of-type(odd)': {
+//             backgroundColor: theme.palette.action.hover,
+//         },
 //     },
-// });
+// }))(TableRow);
+// // const useStyles = makeStyles({
+// //     table: {
+// //         minWidth: 700,
+// //     },
+// // });
 
 //   function descendingComparator(a, b, orderBy) {
 //     if (b[orderBy] < a[orderBy]) {
@@ -59,33 +149,25 @@
 //     return stabilizedThis.map((el) => el[0]);
 //   }
 //   const headCells = [
-//     { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-//     { id: 'position', numeric: true, disablePadding: false, label: 'Position' },
-//     { id: 'company_abbrevation', numeric: true, disablePadding: false, label: 'Company_Abbrevation' },
-//     { id: 'matter_end', numeric: true, disablePadding: false, label: 'Matter_End' },
+//     { id: 'id', numeric: false, disablePadding: true, label: 'スタッフID' },
+//     { id: 'name', numeric: true, disablePadding: false, label: 'スタッフ名' },
+//     { id: 'position', numeric: true, disablePadding: false, label: '役職' },
+//     { id: 'company_abbrevation', numeric: true, disablePadding: false, label: '所属会社' },
+//     { id: 'matter_end', numeric: true, disablePadding: false, label: '案件終了日' },
 //   ];
 
 //   function EnhancedTableHead(props) {
-//     const {  order, orderBy, numSelected, rowCount, onRequestSort } = props;
+//     const { classes, order, orderBy, onRequestSort } = props;
 //     const createSortHandler = (property) => (event) => {
 //       onRequestSort(event, property);
 //     };
-
 //     return (
 //         <TableHead>
-//           <TableRow>
-//             {/* <TableCell padding="checkbox">
-//               <Checkbox
-//                 indeterminate={numSelected > 0 && numSelected < rowCount}
-//                 checked={rowCount > 0 && numSelected === rowCount}
-//                 onChange={onSelectAllClick}
-//                 inputProps={{ 'aria-label': 'select all desserts' }}
-//               /> */}
-//             {/* </TableCell> */}
+//           <StyledTableRow>
 //             {headCells.map((headCell) => (
-//               <TableCell
+//               <StyledTableCell
 //                 key={headCell.id}
-//                 align={headCell.numeric ? 'right' : 'left'}
+//                 align="center"
 //                 padding={headCell.disablePadding ? 'none' : 'default'}
 //                 sortDirection={orderBy === headCell.id ? order : false}
 //               >
@@ -94,16 +176,16 @@
 //                   direction={orderBy === headCell.id ? order : 'asc'}
 //                   onClick={createSortHandler(headCell.id)}
 //                 >
-//                   {/* {headCell.label}
+//                   {headCell.label}
 //                   {orderBy === headCell.id ? (
 //                     <span className={classes.visuallyHidden}>
 //                       {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
 //                     </span>
-//                   ) : null} */}
+//                   ) : null}
 //                 </TableSortLabel>
-//               </TableCell>
+//               </StyledTableCell>
 //             ))}
-//           </TableRow>
+//           </StyledTableRow>
 //         </TableHead>
 //       );
 //     }
@@ -116,7 +198,29 @@
 //         orderBy: PropTypes.string.isRequired,
 //         rowCount: PropTypes.number.isRequired,
 //       };
-
+//       const useStyles = makeStyles((theme) => ({
+//         root: {
+//           width: '100%',
+//         },
+//         paper: {
+//           width: '100%',
+//           marginBottom: theme.spacing(2),
+//         },
+//         table: {
+//           minWidth: 750,
+//         },
+//         visuallyHidden: {
+//           border: 0,
+//           clip: 'rect(0 0 0 0)',
+//           height: 1,
+//           margin: -1,
+//           overflow: 'hidden',
+//           padding: 0,
+//           position: 'absolute',
+//           top: 20,
+//           width: 1,
+//         },
+//       }));
 //     //   const useToolbarStyles = makeStyles((theme) => ({
 //     //     root: {
 //     //       paddingLeft: theme.spacing(2),
@@ -134,33 +238,20 @@
 //     //           },
 //     //     // title: {
 //     //     //   flex: '1 1 100%',
-//     //     // },
+//         // },
 //     //   }));
 
 //     //   const EnhancedTableToolbar = (props) => {
 //     //     const classes = useToolbarStyles();
 //     //     const { numSelected } = props;
 
-//       EnhancedTableToolbar.propTypes = {
-//         numSelected: PropTypes.number.isRequired,
-//       };
+//     //   EnhancedTableToolbar.propTypes = {
+//     //     numSelected: PropTypes.number.isRequired,
+//     //   };
 
 //  export default function StaffList001 () {
 //     const classes = useStyles();
-//     const [order, setOrder] = React.useState('asc');
-//     const [orderBy, setOrderBy] = React.useState('position');
-//     const [selected, setSelected] = React.useState([]);
-//     const [page, setPage] = React.useState(0);
-//     const [dense, setDense] = React.useState(false);
-//     const [rowsPerPage, setRowsPerPage] = React.useState(5);
 //     const [posts, setPosts] = useState([]);
-
-//     const handleRequestSort = (event, property) => {
-//         const isAsc = orderBy === property && order === 'asc';
-//         setOrder(isAsc ? 'desc' : 'asc');
-//         setOrderBy(property);
-//     };
-
 //     useEffect(() => getStaffData());
 //     const getStaffData = () => {
 //         if(posts.length === 0) {
@@ -175,128 +266,124 @@
 //               })
 //             }
 //     }
+//     const [order, setOrder] = React.useState('asc');
+//     const [orderBy, setOrderBy] = React.useState('id');
+//     const [selected, setSelected] = React.useState([]);
+//     const handleRequestSort = (event, property) => {
+//         const isAsc = orderBy === property && order === 'asc';
+//         setOrder(isAsc ? 'desc' : 'asc');
+//         setOrderBy(property);
+//     };
+//     // const [page, setPage] = React.useState(0);
+//     // // const [dense, setDense] = React.useState(false);
+//     // const [rowsPerPage, setRowsPerPage] = React.useState(5);
     
 //     const handleSelectAllClick = (event) => {
 //         if (event.target.checked) {
-//           const newSelecteds = rows.map((n) => n.name);
+//           const newSelecteds = posts.map((n) => n.id);
 //           setSelected(newSelecteds);
 //           return;
 //         }
 //         setSelected([]);
 //       };
 
-//       const handleClick = (event, name) => {
-//         const selectedIndex = selected.indexOf(name);
-//         let newSelected = [];
+//     //   const handleClick = (event, name) => {
+//     //     const selectedIndex = selected.indexOf(name);
+//     //     let newSelected = [];
     
-//         if (selectedIndex === -1) {
-//           newSelected = newSelected.concat(selected, name);
-//         } else if (selectedIndex === 0) {
-//           newSelected = newSelected.concat(selected.slice(1));
-//         } else if (selectedIndex === selected.length - 1) {
-//           newSelected = newSelected.concat(selected.slice(0, -1));
-//         } else if (selectedIndex > 0) {
-//           newSelected = newSelected.concat(
-//             selected.slice(0, selectedIndex),
-//             selected.slice(selectedIndex + 1),
-//           );
-//         }
+//     //     if (selectedIndex === -1) {
+//     //       newSelected = newSelected.concat(selected, name);
+//     //     } else if (selectedIndex === 0) {
+//     //       newSelected = newSelected.concat(selected.slice(1));
+//     //     } else if (selectedIndex === selected.length - 1) {
+//     //       newSelected = newSelected.concat(selected.slice(0, -1));
+//     //     } else if (selectedIndex > 0) {
+//     //       newSelected = newSelected.concat(
+//     //         selected.slice(0, selectedIndex),
+//     //         selected.slice(selectedIndex + 1),
+//     //       );
+//     //     }
 
-//         setSelected(newSelected);
-//     };
+//     //     setSelected(newSelected);
+//     // };
 
-//     const handleChangePage = (event, newPage) => {
-//         setPage(newPage);
-//       };
+//     // const handleChangePage = (event, newPage) => {
+//     //     setPage(newPage);
+//     //   };
 
-//     const handleChangeRowsPerPage = (event) => {
-//         setRowsPerPage(parseInt(event.target.value, 10));
-//         setPage(0);
-//       };
+//     // const handleChangeRowsPerPage = (event) => {
+//     //     setRowsPerPage(parseInt(event.target.value, 10));
+//     //     setPage(0);
+//     //   };
 
-//     const handleChangeDense = (event) => {
-//         setDense(event.target.checked);
-//       };
+//     // const handleChangeDense = (event) => {
+//     //     setDense(event.target.checked);
+//     //   };
     
-//     //   const isSelected = (name) => selected.indexOf(name) !== -1;
+//       const isSelected = (id) => selected.indexOf(id) !== -1;
     
 //     //   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 // return(
 //     <Paper elevation={3} >
 //     <DialogTitle id="customized-dialog-title">
+
 //     <div className='title' style={{ display: 'flex' }}>
-//         <PeopleIcon style={{ fontSize: '40px' }} />
+//         <PeopleIcon style={{ fontSize: '40px', }} />
 //         <Typography style={{ fontSize: '30px' }}>スタッフリスト</Typography>
-//     </div>
+//         </div>
 //     </DialogTitle>
-//     <TableContainer component={Paper}>
-//         <Grid container spacing={24} justify={"center"}>
-//         <Grid className="tablel">
-//       <Table className={classes.table} aria-label="customized table">
-//         <TableHead
+//     {/* <TableContainer component={Paper}> */}
+//         <Grid container spacing={1} alignItems="flex-end">
+//         <Grid item>
+//             <SearchIcon />
+//             </Grid>
+//         <Grid item>
+//             <TextField label="Search" />
+//             </Grid>
+//         </Grid>
+//       <div className={classes.root}>
+//           <Paper className={classes.paper}>
+//               <TableContainer>
+//               <Grid container spacing={24} justify={"center"}>
+//               <Grid className="table1">
+//                   <Table
+//                     className={classes.table}
+//                   >
+//         <EnhancedTableHead
 //           classes={classes}
 //           numSelected={selected.length}
 //           order={order}
 //           orderBy={orderBy}
 //           onSelectAllClick={handleSelectAllClick}
 //           onRequestSort={handleRequestSort}
-//           rowCount={rows.length}>
+//           rowCount={posts.length}
+//           />
+//          <TableBody>
+//           {stableSort(posts, getComparator(order, orderBy))
+//         //    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+//            .map((data, index) => {
+//            const isItemSelected = isSelected(data.id);
+//           //   const labelId = `enhanced-table-checkbox-${index}`;
+//            return (
 //           <TableRow
 //             hover
-//             onClick={(event) => handleClick(event, row.name)}
-//             // role="checkbox"
-//             aria-checked={isItemSelected}
-//             tabIndex={-1}
-//             key={row.name}
-//             selected={isItemSelected}>
-//             <StyledTableCell>スタッフID</StyledTableCell>
-//             <StyledTableCell align="right">スタッフ名</StyledTableCell>
-//             <StyledTableCell align="right">役職</StyledTableCell>
-//             <StyledTableCell align="right">所属会社</StyledTableCell>
-//             <StyledTableCell align="right">案件終了日</StyledTableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//                {/* {stableSort(rows, getComparator(order, orderBy))
-//                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-//                 .map((row, index) => {
-//                   const isItemSelected = isSelected(row.name);
-//                   const labelId = `enhanced-table-checkbox-${index}`; */}
-//                   {/* return (
-//                     // <TableCell component="th" id={labelId} scope="row" padding="none">
-//                     //   {row.name}
-//                     // </TableCell> */}
-
-//           {posts.map((data) => (
-//             <StyledTableRow >
-//             <StyledTableCell component="th" scope="row" aligin="center">
-//                 {data.id}
-//               </StyledTableCell>
-//               <StyledTableCell align="center"><Button>{data.name}</Button></StyledTableCell>
-//               <StyledTableCell align="center">{data.position}</StyledTableCell>
-//               <StyledTableCell align="center">{data.company_abbreviation}</StyledTableCell>
-//               <StyledTableCell align="center">{data.matter_end}</StyledTableCell>
-//             </StyledTableRow>
-//           ))}    
-    
-//         </TableBody>
-//       </Table>
-//       </Grid>
-//       </Grid>    
-//     </TableContainer>
-//     <TablePagination
-//         //   rowsPerPageOptions={[5, 10, 25]}
-//           component="div"
-//           count={rows.length}
-//           rowsPerPage={rowsPerPage}
-//           page={page}
-//           onChangePage={handleChangePage}
-//           onChangeRowsPerPage={handleChangeRowsPerPage}
-//         />
-//    </Paper>
-//     <FormControlLabel
-//         control={<Switch checked={dense} onChange={handleChangeDense} />}
-//         // label="Dense padding"
-//     />
-//  )
-// };
+//            selected={isItemSelected}
+//             >
+//             <TableCell align="center">{data.id}</TableCell>
+//             <TableCell align="center">{data.name}</TableCell>
+//             <TableCell align="center">{data.position}</TableCell>
+//             <TableCell align="center">{data.company_abbreviation}</TableCell>
+//             <TableCell align="center">{data.matter_end}</TableCell>
+//             </TableRow>
+//            );
+//         })}
+//                 </TableBody>
+//                   </Table>
+//               </Grid>
+//               </Grid>
+//               </TableContainer>
+//           </Paper>
+//           </div>
+//           </Paper>
+//           )
+//         };
