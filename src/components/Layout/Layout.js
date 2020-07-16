c89e02cb5460dc4265eb1365deb7992e1240824e
yutowww <wydjent@gmail.com>
2020-07-16 12:36:24 +0900

    pull

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -34,7 +34,7 @@ import CaseSearch from "../../pages/casesearch/CaseSearch";
 //import CaseSearch from "../../pages/casesearch/CaseSearch";
 //import CaseAdd from "../../pages/caseadd/CaseAdd";
 
-import ReferenceList from "../../pages/referenceList/ReferenceList";
+// import ReferenceList from "../../pages/referenceList/ReferenceList";
 import ReferenceList_not from "../../pages/referenceList/notData";
 //import  from "../../pages/";　引合検索
 
@@ -77,7 +77,7 @@ function Layout(props) {
               <Route path="/staff-report/casasearch/001" component={CaseSearch} />
               {/* <Route path="/staff-report/caseadd/001" component={CaseAdd} /> */}
 
-              <Route path="/staff-report/referencelist/001" component={ReferenceList} />
+              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList} /> */}
               <Route path="/staff-report/referencelist/001" component={ReferenceList_not} />
               {/* <Route path="" component={} /> 引合検索  */}
               
