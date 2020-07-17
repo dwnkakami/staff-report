70d352f14861547bb156a3936af4c2ed7e401151
shunichiro <shunichiro.takemura@force-corp.co.jp>
2020-07-17 10:03:58 +0900

    Fix

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -30,13 +30,13 @@ import CaseSearch from "../../pages/casesearch/CaseSearch";
 // import StaffSearch from "../../pages/staffsearch";
 //  import StaffAdd from "../../pages/StaffAdd/Staff addition";
 
-//import CaseList from "../../pages/caselist/CaseList";
-//import CaseSearch from "../../pages/casesearch/CaseSearch";
-//import CaseAdd from "../../pages/caseadd/CaseAdd";
+// import Caselist from "../../pages/caselist/Caselist";
+//import CaseSearch from "../../pages/casesearch";
+//import CaseAdd from "../../pages/caseadd";
+// import CaseDetail from "../../pages/casedetail/CaseDetail";
 
-import ReferenceList from "../../pages/referencelist/ReferenceList";
+import ReferenceList from "../../pages/referenceList/ReferenceList";
 // import ReferenceList_not from "../../pages/referenceList/notData";
-// import ReferenceDetail from "../../pages/referencedetail/ReferenceDetail";
 //import  from "../../pages/";　引合検索
 
 //import Billing from "../../pages/billing/Billing";
@@ -69,22 +69,21 @@ function Layout(props) {
               <Route path="/app/tables" component={Tables} />
               {/* <Route path="/app/tables" component={ReferenceList} /> */}
               <Route path="/app/notifications" component={Notifications} />
-
+​
               {/* <Route path="/staff-report/stafflist/001" component={StaffList001} /> */}
               {/* <Route path="/staff-report/staffsearch/001" component={StaffSearch} /> */}
               {/* { <Route path="/staff-report/staffadd/001" component={StaffAdd} /> } */}
-
-              {/* <Route path="/staff-report/caselist/001" component={CaseList} /> */}
+​
+              {/* <Route path="/staff-report/caselist/001" component={CaseDetail} /> */}
               <Route path="/staff-report/casasearch/001" component={CaseSearch} />
               {/* <Route path="/staff-report/caseadd/001" component={CaseAdd} /> */}
-
+​
               <Route path="/staff-report/referencelist/001" component={ReferenceList} />
               {/* <Route path="/staff-report/referencelist/001" component={ReferenceList_not} /> */}
-              {/* <Route path="/staff-report/referencelist/001" component={ReferenceDetail} /> */}
               {/* <Route path="" component={} /> 引合検索  */}
               
               {/* <Route path="/staff-report/billing/001" component={Billing} /> */}
-
+​
               <Route
                 exact
                 path="/app/ui"
