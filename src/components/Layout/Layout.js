27e8d1fda7489b4dbee2608933415f3bac3ccbfe
jimufree <masahiro.kakami@jimu-free.com>
2020-07-22 11:42:10 +0900

    CaseList merged

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -31,7 +31,7 @@ import CaseSearch from "../../pages/casesearch/CaseSearch";
 // import StaffSearch from "../../pages/staffsearch";
 //  import StaffAdd from "../../pages/StaffAdd/Staff addition";
 
-//import CaseList from "../../pages/caselist/CaseList";
+import CaseList from "../../pages/caselist/CaseList";
 //import CaseSearch from "../../pages/casesearch/CaseSearch";
 //import CaseAdd from "../../pages/caseadd/CaseAdd";
 
@@ -74,7 +74,7 @@ function Layout(props) {
               {/* <Route path="/staff-report/staffsearch/001" component={StaffSearch} /> */}
               <Route path="/staff-report/staffadd/001" component={StaffAdd} />
 
-              {/* <Route path="/staff-report/caselist/001" component={CaseList} /> */}
+              <Route path="/staff-report/caselist/001" component={CaseList} />
               <Route path="/staff-report/casasearch/001" component={CaseSearch} />
               {/* <Route path="/staff-report/caseadd/001" component={CaseAdd} /> */}
 
