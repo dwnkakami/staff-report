eaa5cff0a4ceeb6dffeb914e747c9962aee16807
gennsou <ichigo5932@gmail.com>
2020-07-22 15:33:42 +0900

    commit

--- src/pages/referenceList/ReferenceList.jsx
+++ src/pages/referenceList/ReferenceList.jsx
@@ -43,7 +43,6 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-<<<<<<< HEAD
 function descendingComparator(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
         return -1;
@@ -152,63 +151,58 @@ export default function ReferenceList() {
         if (posts.length === 0) {
             axios
                 .get('/api/referencelist/2')
-=======
-const useStyles = makeStyles({
-    table: {
-        minWidth: 70,
-        
-    },
-});
-
-const DialogTitle = withStyles(styles)((props) => {
-    const { children, classes, onClose, ...other } = props;
-    return (
-        <MuiDialogTitle disableTypography className={classes.root} {...other}>
-            <Typography variant="h6">{children}</Typography>
-        </MuiDialogTitle>
-    );
-});
-
-export default function Referencelist(props) {
-    const classes = useStyles();
-
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => getData());
-
-    const getData = () => {
-        if (posts.length === 0) {
-            axios
-<<<<<<< HEAD
-                .get('/api/referencelist')
-=======
-                .get('/api/referencelist/1')
->>>>>>> 3ce137a9e4ae0a43dbf49982e6670e96441105bb
->>>>>>> 8029f31248e4cd50d6d69948db858819345b8025
                 .then(response => {
                     setPosts(response.data);
                     console.log([response.data])
                 })
-                .catch(() => {
-                    console.log("connected error")
-                })
-        }
-    }
+
+// const useStyles = makeStyles({
+//     table: {
+//         minWidth: 70,
+        
+    },
+}
+
+// const DialogTitle = withStyles(styles)((props) => {
+//     const { children, classes, onClose, ...other } = props;
+//     return (
+//         <MuiDialogTitle disableTypography className={classes.root} {...other}>
+//             <Typography variant="h6">{children}</Typography>
+//         </MuiDialogTitle>
+//     );
+// });
+
+// export default function Referencelist(props) {
+//     const classes = useStyles();
+
+//     const [posts, setPosts] = useState([]);
+
+//     useEffect(() => getData());
+
+//     const getData = () => {
+//         if (posts.length === 0) {
+//             axios
+
+//                 .get('/api/referencelist')
+
+//                 .then(response => {
+//                     setPosts(response.data);
+//                     console.log([response.data])
+//                 })
+//                 .catch(() => {
+//                     console.log("connected error")
+//                 })
+//         }
+//     }
 
     return (
-<<<<<<< HEAD
-        <Paper elevation={3} >
-            <DialogTitle id="customized-dialog-title">
-=======
         <Paper elevation={3}>
                 <DialogTitle id="customized-dialog-title">
->>>>>>> 8029f31248e4cd50d6d69948db858819345b8025
                 <div className='title' style={{ display: 'flex' }}>
                     <DescriptionOutlinedIcon style={{ fontSize: '40px', }} />
                     <Typography style={{ fontSize: '30px' }}>引合リスト</Typography>
                 </div>
             </DialogTitle>
-<<<<<<< HEAD
 
             <div className={classes.root}>
                 <Paper className={classes.paper}>
@@ -262,42 +256,3 @@ export default function Referencelist(props) {
         </Paper>
     )
 };
-=======
-            
-             <Divider className="line" />
-
-
-            <TableContainer >
-                <Grid container spacing={24} justify={"center"}>
-                    <Grid className="table1">
-                        <Table className={classes.table} aria-label="customized table">
-                            <TableHead>
-                                <TableRow>
-                                    <StyledTableCell>案件名</StyledTableCell>
-                                    <StyledTableCell align="left">スタッフ名</StyledTableCell>
-                                    <StyledTableCell align="left">発注元会社</StyledTableCell>
-                                    <StyledTableCell align="left">状態</StyledTableCell>
-                                    <StyledTableCell align="left">詳細</StyledTableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {posts.map((data) => (
-                                    <StyledTableRow>
-                                        <StyledTableCell component="th" scope="row">
-                                            {data.name}
-                                        </StyledTableCell>
-                                        <StyledTableCell align="left">{data.staff_id}</StyledTableCell>
-                                        <StyledTableCell align="left">{data.customer_abbreviation}</StyledTableCell>
-                                        <StyledTableCell align="left"></StyledTableCell>
-                                        <StyledTableCell align="left">{data.note}</StyledTableCell>
-                                    </StyledTableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </Grid>
-                </Grid>
-            </TableContainer>
-        </Paper>
-    );
-}
->>>>>>> 8029f31248e4cd50d6d69948db858819345b8025
