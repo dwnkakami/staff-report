9eb6747ed033fdbce2375a5106689084d15e0cfa
吉田梨絵 <rieyoshida@yoshidarienoMacBook-ea.local>
2020-07-27 11:26:19 +0900

    stafflist

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -27,7 +27,7 @@ import StaffAdd from "../../pages/staffadd/StaffAdd";
 
 import CaseSearch from "../../pages/casesearch/CaseSearch";
 
-// import StaffList001 from "../pages/stafflist001";
+import StaffList001 from "../../pages/stafflist001/StaffList001";
 // import StaffSearch from "../../pages/staffsearch";
 //  import StaffAdd from "../../pages/StaffAdd/Staff addition";
 
@@ -70,7 +70,7 @@ function Layout(props) {
               {/* <Route path="/app/tables" component={ReferenceList} /> */}
               <Route path="/app/notifications" component={Notifications} />
 
-              {/* <Route path="/staff-report/stafflist/001" component={StaffList001} /> */}
+              <Route path="/staff-report/stafflist/001" component={StaffList001} />
               {/* <Route path="/staff-report/staffsearch/001" component={StaffSearch} /> */}
               <Route path="/staff-report/staffadd/001" component={StaffAdd} />
 
