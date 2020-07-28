0b4b5e68a67a1b282abf76839fc02e231f5296ec
jimufree <masahiro.kakami@jimu-free.com>
2020-07-28 10:20:15 +0900

    StaffSearch merged

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -28,13 +28,14 @@ import CaseSearch from "../../pages/casesearch/CaseSearch";
 
 // import StaffList001 from "../pages/stafflist001";
 import StaffSearch from "../../pages/staffsearch/StaffSearch";
+import StaffList_result from "../../pages/staffsearch/StaffList_result";
 //  import StaffAdd from "../../pages/StaffAdd/Staff addition";
 
 import CaseList from "../../pages/caselist/CaseList";
 //import CaseSearch from "../../pages/casesearch/CaseSearch";
 import CaseAdd from "../../pages/caseadd/CaseAdd";
 
-// import ReferenceList from "../../pages/referenceList/ReferenceList";
+import ReferenceList from "../../pages/referenceList/ReferenceList";
 import ReferenceList_not from "../../pages/referenceList/notData";
 //import  from "../../pages/";　引合検索
 
@@ -71,14 +72,14 @@ function Layout(props) {
 
               <Route path="/staff-report/stafflist/001" component={StaffList001} />
               <Route path="/staff-report/staffsearch/001" component={StaffSearch} />
-              {/* <Route path="/staff-report/staffadd/001" component={StaffAdd} /> */}
+              <Route path="/staff-report/stafflist-result/001" component={StaffList_result} />
               <Route path="/staff-report/staffadd/001" component={StaffAdd} />
 
               <Route path="/staff-report/caselist/001" component={CaseList} />
               <Route path="/staff-report/casasearch/001" component={CaseSearch} />
               <Route path="/staff-report/caseadd/001" component={CaseAdd} />
 
-              {/* <Route path="/staff-report/referencelist/001" component={ReferenceList} /> */}
+              <Route path="/staff-report/referencelist/001" component={ReferenceList} />
               <Route path="/staff-report/referencelist/001" component={ReferenceList_not} />
               {/* <Route path="" component={} /> 引合検索  */}
               
