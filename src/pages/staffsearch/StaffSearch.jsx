a5c8bf5ce0998ce412213138fd5c9526484ce629
Gwey-Masao <masao.guei@force-corp.co.jp>
2020-08-26 18:25:39 +0900

    and条件モード

--- src/pages/staffsearch/StaffSearch.jsx
+++ src/pages/staffsearch/StaffSearch.jsx
@@ -387,24 +387,24 @@ const StaffSearch = () => {
               (data.age === age) ||
               (data.area === areas);
       }
-      else if(data.length === 0 && andMode)
+      else
       {
-        return (data.occupation === ocp[0]) ||
-          (data.occupation === ocp[1]) ||
-          (data.occupation === ocp[2]) ||
-          (data.occupation === ocp[3]) ||
-          (data.occupation === ocp[4]) ||
-          (data.occupation === ocp[5]) ||
-         (data.license === license) ||
-         ((data.skill === skill1) && 
-          (data.level === status1)) ||
+        return (data.occupation === ocp[0] ||
+          data.occupation === ocp[1] ||
+          data.occupation === ocp[2] ||
+          data.occupation === ocp[3] ||
+          data.occupation === ocp[4] ||
+          data.occupation === ocp[5]) &&
+          ((data.skill === skill1) && 
+           (data.level === status1)) ||
           ((data.skill === skill2) && 
-          (data.level === status2)) ||
+           (data.level === status2)) ||
           ((data.skill === skill3) && 
-          (data.level === status3)) ||
-         (data.gender === ge) ||
-         (data.age === age) ||
-         (data.area === areas);
+           (data.level === status3)) ||
+          (data.license === license) ||
+          (data.gender === ge) ||
+          (data.age === age) ||
+          (data.area === areas);
       }
     });
   
